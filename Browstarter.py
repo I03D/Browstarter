5a21a59468808e6ab1c44c9b36183f9d0068f690
I03D <98839825+I03D@users.noreply.github.com>
2024-03-10 14:32:23 +0700

    Update Browstarter.py

--- Browstarter.py
+++ Browstarter.py
@@ -8,9 +8,9 @@ elif prompt[:3] == "yt ":
     os.system("ungoogled-chromium --kiosk --new-window \"https://youtube.com/results?search_query="+prompt[3:].replace(" ", "+")+"\"")
 elif prompt[:3] in ["tr ", "тр "]:
     if prompt[4].lower() in "абвгдеёжзийклмнопрстуфхцчшщъыьэюя":
-        os.system("ungoogled-chromium --kiosk --new-window \"https://translate.yandex.ru/?source_lang=ru&target_lang=en&text="+prompt[3:].replace(" ", "+")+"\"")
+        os.system("chromium --kiosk --new-window \"https://translate.yandex.ru/?source_lang=ru&target_lang=en&text="+prompt[3:].replace(" ", "+")+"\"")
     else:
-        os.system("ungoogled-chromium --kiosk --new-window \"https://translate.yandex.ru/?source_lang=en&target_lang=ru&text="+prompt[3:].replace(" ", "+")+"\"")
+        os.system("chromium --kiosk --new-window \"https://translate.yandex.ru/?source_lang=en&target_lang=ru&text="+prompt[3:].replace(" ", "+")+"\"")
 
 else:
     os.system("ungoogled-chromium --kiosk --new-window \"https://google.com/search?q="+prompt.replace(" ", "+")+"\"")
