c4196b2b90b2dfe301b138de524d4113ff3cfa00
I03D <98839825+I03D@users.noreply.github.com>
2024-03-14 21:05:21 +0700

    Update Browstarter.py

--- Browstarter.py
+++ Browstarter.py
@@ -3,16 +3,16 @@ import os
 prompt = input("Введите запрос: ")
 
 if "." in prompt:
-    os.system("nohup chromium --app="+prompt+" &")
+    os.system("nohup chromium --app="+prompt+">>/dev/null &")
 elif prompt[:3] == "yt ":
-    os.system("nohup chromium --app=https://youtube.com/results?search_query="+prompt[3:].replace(" ", "+")+" &")
+    os.system("nohup chromium --app=https://youtube.com/results?search_query="+prompt[3:].replace(" ", "+")+">>/dev/null &")
 elif prompt[:3] in ["tr ", "тр "]:
     if prompt[4].lower() in "абвгдеёжзийклмнопрстуфхцчшщъыьэюя":
-        os.system("nohup chromium --app=https://translate.yandex.ru/?source_lang=ru&target_lang=en&text="+prompt[3:].replace(" ", "+")+" &")
+        os.system("nohup chromium --app=https://translate.yandex.ru/?source_lang=ru&target_lang=en&text="+prompt[3:].replace(" ", "+")+">>/dev/null &")
     else:
-        os.system("nohup chromium --app=https://translate.yandex.ru/?source_lang=en&target_lang=ru&text="+prompt[3:].replace(" ", "+")+" &")
+        os.system("nohup chromium --app=https://translate.yandex.ru/?source_lang=en&target_lang=ru&text="+prompt[3:].replace(" ", "+")+">>/dev/null &")
 
 else:
-    os.system("nohup chromium --app=https://google.com/search?q="+prompt.replace(" ", "+")+" &")
+    os.system("nohup chromium --app=https://google.com/search?q="+prompt.replace(" ", "+")+">>/dev/null &")
 quit()
 
